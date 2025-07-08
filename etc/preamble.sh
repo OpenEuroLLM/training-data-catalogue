cd4827d333fb827456fb22fafb83abcc4d35af4d
Stephan Oepen <oe@ifi.uio.no>
2025-07-08 03:03:07 +0300

    robustness

--- etc/preamble.sh
+++ etc/preamble.sh
@@ -1,23 +1,25 @@
 set -x;
 
-if [ -n "${SLURM_JOB_ID}" ]; then
-  script=$(scontrol show job ${SLURM_JOB_ID} \
-  | awk '/^ *Command=/ {sub(/^ *Command=/, "", $1); print $1;}');
-  path=$(dirname ${script});
-  ROOT="${path%/etc}";
-else
-  path=$(dirname $0);
-  if [ "${path#./}" != "${path}" ]; then
-    path="$(pwd)/${path#./}";
-  fi
-  if [ "${path#/}" = "${path}" ]; then
-    if [ "${path}" = "." ]; then
-      path="$(pwd)";
-    else 
-      path="$(pwd)/${path}";
+if [ -z "${ROOT}" ]; then
+  if [ -n "${SLURM_JOB_ID}" ]; then
+    script=$(scontrol show job ${SLURM_JOB_ID} \
+    | awk '/^ *Command=/ {sub(/^ *Command=/, "", $1); print $1;}');
+    path=$(dirname ${script});
+    ROOT="${path%/etc}";
+  else
+    path=$(dirname $0);
+    if [ "${path#./}" != "${path}" ]; then
+      path="$(pwd)/${path#./}";
     fi
+    if [ "${path#/}" = "${path}" ]; then
+      if [ "${path}" = "." ]; then
+	path="$(pwd)";
+      else 
+	path="$(pwd)/${path}";
+      fi
+    fi
+    ROOT="${path%/etc}";
   fi
-  ROOT="${path%/etc}";
 fi
 ROOT=$(realpath ${ROOT});
 BASE=$(realpath ${ROOT}/../..);
