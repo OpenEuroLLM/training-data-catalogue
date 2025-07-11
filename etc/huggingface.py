2a3796f63e9cb3892f7e40d294a2f479498b3d2e
Stephan Oepen <oe@ifi.uio.no>
2025-07-11 18:02:24 +0200

    bug fix

--- etc/huggingface.py
+++ etc/huggingface.py
@@ -1,6 +1,5 @@
 def api_retrieve_files(path, revision, pattern):
   import json, re, urllib.request;
-  print(f"https://huggingface.co/api/datasets/{path}/revision/${revision}");
   with urllib.request.urlopen(f"https://huggingface.co/api/datasets/{path}/revision/{revision}") as stream:
     for file in json.load(stream)["siblings"]:
       name = file["rfilename"];
