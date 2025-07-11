6f8a49c86560be02f14b93c61e0eb422d839009b
Stephan Oepen <oe@ifi.uio.no>
2025-07-11 17:36:59 +0200

    extended api

--- etc/huggingface.py
+++ etc/huggingface.py
@@ -1,7 +1,6 @@
-def retrieve_files(path, pattern):
-  import re;
+def api_retrieve_files(path, revision, pattern):
   import json, re, urllib.request;
-  with urllib.request.urlopen(f"https://huggingface.co/api/datasets/{path}") as stream:
+  with urllib.request.urlopen(f"https://huggingface.co/api/datasets/{path}/revision/${revision}") as stream:
     for file in json.load(stream)["siblings"]:
       name = file["rfilename"];
       if re.search(pattern, name): print(name);
