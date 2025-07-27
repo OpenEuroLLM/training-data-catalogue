654d2f5b7c5cf8fc44e462fbf987aca5578e7d11
Stephan Oepen <oe@ifi.uio.no>
2025-07-27 17:37:30 +0200

    recursive indexing

--- etc/index.py
+++ etc/index.py
@@ -12,7 +12,7 @@ import time;
 from urllib.parse import urlsplit;
 import zstandard as zstd;
 
-def index_file(path, text = "text", url = "u", level = 1, write = True):
+def index_file(path, text = "text", url = "u", level = 1):
 
   stream = None;
   if path.endswith(".zst") or path.endswith(".zstd"):
@@ -58,38 +58,50 @@ def index_file(path, text = "text", url = "u", level = 1, write = True):
       index(domain, domains);
       index(address, urls);
 
-  if write:
-    def output(dictionary, suffix):
-      name = os.path.join(directory, "." + file + suffix + ".zst");
-      compressor = zstd.ZstdCompressor(level = 10, threads = 1);
-      stream = compressor.stream_writer(open(name, "wb"));
-      stream = io.TextIOWrapper(stream, encoding = "utf-8", errors = "replace");
-      for _, __ in sorted(dictionary.items()):
-        print("{}\t{}".format(_, __["n"]),
-              end = "\t", file = stream);
-        __.pop("n");
-        json.dump(__, stream);
-        print(file = stream);
-      stream.close();
+  def output(dictionary, suffix):
+    name = os.path.join(directory, "." + file + suffix + ".zst");
+    compressor = zstd.ZstdCompressor(level = 10, threads = 1);
+    stream = compressor.stream_writer(open(name, "wb"));
+    stream = io.TextIOWrapper(stream, encoding = "utf-8", errors = "replace");
+    for _, __ in sorted(dictionary.items()):
+      print("{}\t{}".format(_, __["n"]),
+            end = "\t", file = stream);
+      __.pop("n");
+      json.dump(__, stream);
+      print(file = stream);
+    stream.close();
 
-    for _ in (".zstd", ".zst", ".gz", ".jsonl", ".json"):
-      if file.endswith(_): file = file[:-len(_)];
-    output(signatures, ".signatures");
-    if url is not None:
-      output(domains, ".domains");
-      output(urls, ".urls");
+  for _ in (".zstd", ".zst", ".gz", ".jsonl", ".json"):
+    if file.endswith(_): file = file[:-len(_)];
+  output(signatures, ".signatures");
+  if url is not None:
+    output(domains, ".domains");
+    output(urls, ".urls");
 
-    return i + 1;
-  else:
-    return domains, urls, signatures;
+  return i + 1;
       
-def index_directory(path, pattern = "\\.jsonl\\.zst$", cores = 1, text = "text", url = "u", level = 1):
+def index_directory(path, pattern = r"\.jsonl\.zst$", cores = 1,
+                    text = "text", url = "u", level = 1, tree = False):
+
+  pattern = re.compile(pattern);
+  def walk(path, tree):
+    if not os.path.isdir(path):
+      print(f"merge.py: ignoring invalid path {path}.",
+            file = sys.stderr);
+      return [];
+    result = [];
+    for path in glob.glob(os.path.join(path, "*")):
+      if tree and os.path.isdir(path):
+        result += walk(path, tree);
+      elif pattern.search(path) and os.path.isfile(path):
+        result.append(path);
+    return result;
+
   start = time.time();
   with mp.Pool(cores) as pool:
     counts = pool.starmap(index_file,
                           ((file, text, url, level)
-                           for file in glob.glob(os.path.join(path, "*"))
-                           if re.search(pattern, file)));
+                           for file in walk(path, tree)));
   print("index.py: processed {} files; {} documents; {:.2f} seconds."
         "".format(len(counts), sum(counts), time.time() - start));
 
@@ -108,71 +120,8 @@ def index_directory(path, pattern = "\\.jsonl\\.zst$", cores = 1, text = "text",
       inputs.append((key, count, input));
     return inputs;
   #
-  # parse one tab-separated entry from an index file
-  #
-  def parse(input):
-    line = next(input["stream"], None);
-    if line is None:
-      input["stream"].close();
-      return None, None, None;
-    input["n"] += 1;
-    try:
-      _ = line.find("\t");
-      key = line[:_];
-      line = line[_ + 1:];
-      _ = line.find("\t");
-      count = line[:_];
-      entry = json.loads(line[_ + 1:]);
-    except Exception as error:
-      print("index.py: aborting input from {file}, #{}: {error}."
-            "".format(input["file"], input["n"], error),
-            file = sys.stderr);
-      input["stream"].close();
-      return None, None, None;
-    input["entry"] = entry;
-    return key, count, input;
   #
   #
-  #
-  def merge(inputs, stream):
-    n = 0;
-    while len(inputs):
-      #
-      # _fix_me_ should use a genuine priority queue
-      # 
-      inputs.sort(key = itemgetter(0));
-      key, count, input = inputs.pop();
-      #
-      # process other (currently visible) entries with the same key
-      #
-      for i in range(len(inputs), 0, -1):
-        if inputs[i - 1][0] == key:
-          #
-          # merge count and payload of matching entry
-          #
-          match = inputs.pop();
-          count += match[1];
-          input["entry"].update(match[2]["entry"]);
-          #
-          # update for next record and re-queue, unless exhausted
-          #
-          key, count, match = parse(match);
-          if key is None: continue;
-          else: inputs.append((key, count, match));
-        else: break;
-      print(f"{key}\t{count}", end = "\t", file = stream);
-      json.dump(input["entry"], stream);
-      print(file = stream);
-      n += 1;
-      #
-      # update next key, count, and entry from this input file;
-      # re-insert into the priority queue, unless exhausted
-      #
-      key, count, input = parse(input);
-      if key is None: continue;
-      else: inputs.append((key, count, input));
-
-    return n;
 
   def compress(suffix):
     name = os.path.join(path, "." + suffix + ".zst");
@@ -192,4 +141,74 @@ def index_directory(path, pattern = "\\.jsonl\\.zst$", cores = 1, text = "text",
     for _ in inputs: _["stream"].close();
   print("index.py: merged {} files; {} records; {:.2f} seconds."
         "".format(n, r, time.time() - start));
-    
+
+def parse(input):
+  #
+  # parse one tab-separated entry from an index file
+  #
+  line = next(input["stream"], None);
+  if line is None:
+    input["stream"].close();
+    return None, None, None;
+  input["n"] += 1;
+  try:
+    _ = line.find("\t");
+    key = line[:_];
+    line = line[_ + 1:];
+    _ = line.find("\t");
+    count = line[:_];
+    entry = json.loads(line[_ + 1:]);
+  except Exception as error:
+    print("index.py: aborting input from {file}, #{}: {error}."
+          "".format(input["file"], input["n"], error),
+          file = sys.stderr);
+    input["stream"].close();
+    return None, None, None;
+  input["entry"] = entry;
+  return key, count, input;
+
+def merge(inputs, stream):
+  #
+  # sorted merge set of records from a set of input streams
+  #
+  n = 0;
+  while len(inputs):
+    #
+    # _fix_me_ should use a genuine priority queue
+    # 
+    inputs.sort(key = itemgetter(0));
+    key, count, input = inputs.pop();
+    #
+    # process other (currently visible) entries with the same key
+    #
+    for i in range(len(inputs), 0, -1):
+      if inputs[i - 1][0] == key:
+        #
+        # merge count and payload of matching entry
+        #
+        match = inputs.pop();
+        count += match[1];
+        input["entry"].update(match[2]["entry"]);
+        #
+        # update for next record and re-queue, unless exhausted
+        #
+        key, count, match = parse(match);
+        if key is None: continue;
+        else: inputs.append((key, count, match));
+      else: break;
+    print(f"{key}\t{count}", end = "\t", file = stream);
+    json.dump(input["entry"], stream);
+    print(file = stream);
+    n += 1;
+    #
+    # update next key, count, and entry from this input file;
+    # re-insert into the priority queue, unless exhausted
+    #
+    key, count, input = parse(input);
+    if key is None: continue;
+    else: inputs.append((key, count, input));
+
+  return n;
+
+def compare_directories(left, right):
+  1;
